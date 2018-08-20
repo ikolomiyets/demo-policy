bfde2ee8e9d50470f4ec1ef324eec5446841de8a
Igor Kolomiyets <ikolomiyets@gmail.com>
2018-08-20 22:27:37 +0100

    Taking a different approach, excluding certain classes from the auto-configuration

--- src/main/java/io/iktech/demo/PolicyDemoApplication.java
+++ src/main/java/io/iktech/demo/PolicyDemoApplication.java
@@ -1,47 +1,20 @@
 package io.iktech.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
-import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
-import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
-import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.*;
-import org.springframework.boot.autoconfigure.webservices.WebServicesAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
-import org.springframework.boot.devtools.autoconfigure.LocalDevToolsAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Configuration
-@EnableScheduling
-@Import({
-        DispatcherServletAutoConfiguration.class,
-        EmbeddedServletContainerAutoConfiguration.class,
-        ErrorMvcAutoConfiguration.class,
-        CacheAutoConfiguration.class,
-        HttpEncodingAutoConfiguration.class,
-        HttpMessageConvertersAutoConfiguration.class,
-        JacksonAutoConfiguration.class,
-        JmxAutoConfiguration.class,
-        LocalDevToolsAutoConfiguration.class,
-        MultipartAutoConfiguration.class,
-        PropertyPlaceholderAutoConfiguration.class,
-        ServerPropertiesAutoConfiguration.class,
-        SpringApplicationAdminJmxAutoConfiguration.class,
-        ValidationAutoConfiguration.class,
-        JerseyAutoConfiguration.class,
-        WebClientAutoConfiguration.class,
-        WebMvcAutoConfiguration.class,
+@SpringBootApplication(exclude = {
         WebSocketAutoConfiguration.class,
-        WebServicesAutoConfiguration.class,
-        AppInsightsConfig.class
+        MultipartAutoConfiguration.class,
+        WebClientAutoConfiguration.class,
+        ValidationAutoConfiguration.class
 })
-@ComponentScan({"io.iktech.demo"})
+@EnableScheduling
 public class PolicyDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(PolicyDemoApplication.class, args);
