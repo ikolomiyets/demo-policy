32088ff2ce578d1a50152e85536b8f88a8aaaac4
Igor Kolomiyets <ikolomiyets@gmail.com>
2018-08-20 20:02:11 +0100

    Removed autoconfiguration

--- src/main/java/io/iktech/demo/PolicyDemoApplication.java
+++ src/main/java/io/iktech/demo/PolicyDemoApplication.java
@@ -1,10 +1,54 @@
 package io.iktech.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.*;
+import org.springframework.boot.autoconfigure.webservices.WebServicesAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.devtools.autoconfigure.LocalDevToolsAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
-@SpringBootApplication
+
+import java.util.List;
+
+@Configuration
 @EnableScheduling
+@Import({
+        DispatcherServletAutoConfiguration.class,
+        EmbeddedServletContainerAutoConfiguration.class,
+        ErrorMvcAutoConfiguration.class,
+        CacheAutoConfiguration.class,
+        HttpEncodingAutoConfiguration.class,
+        HttpMessageConvertersAutoConfiguration.class,
+        JacksonAutoConfiguration.class,
+        JmxAutoConfiguration.class,
+        LocalDevToolsAutoConfiguration.class,
+        MultipartAutoConfiguration.class,
+        PropertyPlaceholderAutoConfiguration.class,
+        ServerPropertiesAutoConfiguration.class,
+        SpringApplicationAdminJmxAutoConfiguration.class,
+        ValidationAutoConfiguration.class,
+        WebClientAutoConfiguration.class,
+        WebMvcAutoConfiguration.class,
+        WebSocketAutoConfiguration.class,
+        WebServicesAutoConfiguration.class
+})
+@ComponentScan({"io.iktech.demo"})
 public class PolicyDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(PolicyDemoApplication.class, args);
