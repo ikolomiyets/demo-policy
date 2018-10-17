eaaa5e1b9b46ff9f590782d91cfc760c4c47f503
Igor Kolomiyets <ikolomiyets@gmail.com>
2018-10-17 19:53:00 +0100

    Upgraded application to JDK 11

--- src/main/java/io/iktech/demo/PolicyDemoApplication.java
+++ src/main/java/io/iktech/demo/PolicyDemoApplication.java
@@ -3,13 +3,11 @@ package io.iktech.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration;
-import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {
-        WebSocketAutoConfiguration.class,
         MultipartAutoConfiguration.class,
         WebClientAutoConfiguration.class,
         ValidationAutoConfiguration.class
